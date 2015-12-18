1615c138adbebe8bfaf07c597c8fcdf84501b452
Chr0nos <snicolet@student.42.fr>
2015-12-18 19:13:23 +0100

    ameliration du binarizator

--- binarizator_of_doom.c
+++ binarizator_of_doom.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 13:52:34 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/18 15:14:40 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/18 19:12:55 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,29 @@
 
 unsigned short	binarizator_of_doom(t_element *t)
 {
-	unsigned short	b;
-	unsigned char	p;
+	unsigned short			b;
+	unsigned int			pos;
+	unsigned short			x;
+	unsigned short			pow;
+	const unsigned short	endpos = (unsigned short)(t->height * t->width);
 
 	b = 0;
-	p = 0;
-	while (p < t->height)
+	pos = 0;
+	pow = 1;
+	x = 0;
+	while (pos < endpos)
 	{
-		
-		p++;
+		if (x % t->width == 0)
+		{
+			++x;
+			while (--x)
+				pow *= 2;
+		}
+		if (t->data[pos / t->height][pos % t->width] != '.')
+			b += pow;
+		pow *= 2;
+		++x;;
+		++pos;
 	}
 	return (b);
 }
