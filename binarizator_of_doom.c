a3e627da0dde7712943d983a5068f85f09eac41e
Chr0nos <snicolet@student.42.fr>
2015-12-22 18:20:48 +0100

    binarizator

--- binarizator_of_doom.c
+++ binarizator_of_doom.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 13:52:34 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/22 18:17:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/22 18:20:20 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,13 @@ unsigned short	binarizator_of_doom(t_element *t)
 	col = 0;
 	pow = 1;
 	col = t->width + 1;
-	while ((--col) && (line = t->height + 1))
+	while ((--col) && (line = t->height + 1) && (x = 4 - t->width))
 	{
 		while (line)
 		{
 			if ((++pow) && (t->data[--line][col] != '.'))
 				b += ft_pow(2, pow) / 2;
 		}
-		x = 4 - t->width;
 		while (x)
 			if (++pow)
 				x--;
