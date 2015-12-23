d4ef887ac3431e4c20747a18d883f7946e13c3da
Chr0nos <snicolet@student.42.fr>
2015-12-23 15:50:56 +0100

    amelioration de la fonction d insertion

--- trouvator.c
+++ trouvator.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 15:03:43 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/23 15:35:39 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/23 15:50:26 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,34 +27,44 @@ static unsigned char	revbits(unsigned char b)
 	return (x);
 }
 
-static int			insert_bin(t_fillit *f, int n)
+static int			insert_bin(t_fillit *f, unsigned int n)
 {
 	unsigned char		b;
+	const unsigned char	bo = revbits((unsigned char)f->elems[n].bin);
 	unsigned int		p;
 	const unsigned int	end = f->grid_size * f->grid_size;
 
-	b = revbits((unsigned char)f->elems[n].bin);
+	b = bo;
 	p = 0;
 	while (p < end)
 	{
+		while (((f->bgrid[p] & b) != 0) && ((b & 1) == 0))
+			b >>= 1;
 		if ((f->bgrid[p] & b) == 0)
 		{
-			f->bgrid[p] = b;
+			f->bgrid[p] |= b;
 			return (1);
 		}
+		b = bo;
 		p++;
 	}
 	return (0);
 }
 
-static int			trouvator_engine(t_fillit *x, int p, int n)
+static int			trouvator_engine(t_fillit *x, unsigned int n)
 {
 	int		ret;
 
 	ret = insert_bin(x, n);
-	(void)p;
-	(void)ret;
-	return (1);
+	if (ret == 1)
+	{
+		if (n + 1 < x->elements_count)
+			return (trouvator_engine(x, n + 1));
+		else
+			return (1);
+	}
+	else
+		return (0);
 }
 
 int					trouvator(t_list *lst)
@@ -65,7 +75,7 @@ int					trouvator(t_list *lst)
 	ft_lstdel(&lst, ft_lstpulverisator);
 	if (!fillit)
 		return (-1);
-	trouvator_engine(fillit, 0, 0);
+	trouvator_engine(fillit, 0);
 	displayator(fillit);
 	liberator(fillit);
 	return (0);
