fcf20af0753485bd820b1a45cb585d719ed748d7
Chr0nos <snicolet@student.42.fr>
2015-12-26 12:06:02 +0100

    bug du removator presque fixe

--- removator.c
+++ removator.c
@@ -6,29 +6,28 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/20 14:11:45 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/26 11:50:56 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/26 12:05:24 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "fillit.h"
 
-static unsigned short rmmask(unsigned short b, unsigned short mask)
-{
-	return (b & ~mask);
-}
-
 void					removator(t_fillit *x, unsigned int n)
 {
 	const unsigned short	mask = x->elems[n].mask;
 	unsigned short			b;
 	unsigned short			p;
+	unsigned short			submask;
 
-	p = 4;
-	while (p--)
+	submask = 15;
+	p = 0;
+	while (p < x->elems[n].height)
 	{
 		b = x->bgrid[x->elems[n].pos + p];
-		x->bgrid[x->elems[n].pos + p] = rmmask(b, mask >> (4 * p));
+		x->bgrid[x->elems[n].pos + p] = (b & (mask & submask));
+		submask <<= 4;
+		p++;
 	}
 	x->elems[n].mask = 0;
 }
