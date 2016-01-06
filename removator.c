8dd50130f272cf5e81940c5c1eb1805d5000caea
Chr0nos <snicolet@student.42.fr>
2016-01-06 23:22:47 +0000

    reduction du code du removator

--- removator.c
+++ removator.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/20 14:11:45 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/05 16:04:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 23:22:15 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,10 @@
 
 int					removator(t_fillit *f, t_element *elem)
 {
-	unsigned short			b;
-	unsigned char			p;
-	unsigned char			x;
+	const unsigned short	b = elem->bin;
+	const unsigned char		p = elem->pos >> 8;
+	unsigned char			x = (unsigned char)(elem->pos & 255);
 
-	b = elem->bin;
-	p = elem->pos >> 8;
-	x = (unsigned char)(elem->pos & 255);
 	f->bgrid[p] ^= (b & 61440) >> x;
 	f->bgrid[p + 1] ^= ((b << 4) & 61440) >> x;
 	f->bgrid[p + 2] ^= ((b << 8) & 61440) >> x;
