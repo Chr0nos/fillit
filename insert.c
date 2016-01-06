dc2f57fe04dcb5e25eb205e88998461a70da1f33
Chr0nos <snicolet@student.42.fr>
2016-01-06 23:16:39 +0000

    clarification du code

--- insert.c
+++ insert.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/24 17:33:28 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/06 23:13:15 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 23:16:15 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,14 @@ int		insert_bin(t_fillit *f, int x, int y, t_element *elem)
 {
 	const unsigned short	*m = elem->masks;
 
-	if (canfit(f, x, y, elem))
-	{
-		elem->pos = (unsigned short)((unsigned short)(y) << 8);
-		elem->pos |= (unsigned short)x;
-		f->bgrid[y] |= m[0] >> x;
-		f->bgrid[y + 1] |= m[1] >> x;
-		f->bgrid[y + 2] |= m[2] >> x;
-		f->bgrid[y + 3] |= m[3] >> x;
-		elem->placed = 1;
-		return (1);
-	}
-	return (0);
+	if (!canfit(f, x, y, elem))
+		return (0);
+	elem->pos = (unsigned short)((unsigned short)(y) << 8);
+	elem->pos |= (unsigned short)x;
+	f->bgrid[y] |= m[0] >> x;
+	f->bgrid[y + 1] |= m[1] >> x;
+	f->bgrid[y + 2] |= m[2] >> x;
+	f->bgrid[y + 3] |= m[3] >> x;
+	elem->placed = 1;
+	return (1);
 }
