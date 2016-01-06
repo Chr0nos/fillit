ab371b9fb37424d90e77e724f8ef2b3be97366cb
Chr0nos <snicolet@student.42.fr>
2016-01-06 14:07:13 +0100

    optimisation

--- canfit.c
+++ canfit.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/28 13:59:10 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/05 18:48:23 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 13:58:18 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static int	canfit_horizontal(t_fillit *f, int x, int y, t_element *bloc)
 	unsigned short	mask;
 
 	mask = (bloc->bin & 61440) >> x;
-	while (mask & f->bgrid[y])
+	if (mask & f->bgrid[y])
 	{
 		mask >>= 1;
 		if (((++x) + bloc->width) > (int)f->grid_size)
@@ -43,7 +43,7 @@ static int	canfit_horizontal(t_fillit *f, int x, int y, t_element *bloc)
 
 int			canfit(t_fillit *f, int x, int y, t_element *elem)
 {
-	while ((x = canfit_horizontal(f, x, y, elem)) != -1)
+	if ((x = canfit_horizontal(f, x, y, elem)) != -1)
 	{
 		if (canfit_bloc(f, x, y, elem))
 			return (x);
--- insert.c
+++ insert.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/24 17:33:28 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/05 18:43:35 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 14:05:21 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,22 @@
 int						insert_bin(t_fillit *f, int x, int y, t_element *elem)
 {
 	const unsigned short	bo = elem->bin;
-	int						p;
 
-	p = y;
-	while ((size_t)p + elem->height <= f->grid_size)
+	if ((size_t)y + elem->height <= f->grid_size)
 	{
-		x = canfit(f, x, p, elem);
-		f->bgrid[p] &= ((unsigned short)65535) << (16 - f->grid_size);
+		x = canfit(f, x, y, elem);
+		f->bgrid[y] &= ((unsigned short)65535) << (16 - f->grid_size);
 		if (x > -1)
 		{
-			elem->pos = (unsigned short)((unsigned short)(p) << 8);
+			elem->pos = (unsigned short)((unsigned short)(y) << 8);
 			elem->pos |= (unsigned short)x;
-			f->bgrid[p] |= (bo & 61440) >> x;
-			f->bgrid[p + 1] |= ((bo << 4) & 61440) >> x;
-			f->bgrid[p + 2] |= ((bo << 8) & 61440) >> x;
-			f->bgrid[p + 3] |= ((bo << 12) & 61440) >> x;
+			f->bgrid[y] |= (bo & 61440) >> x;
+			f->bgrid[y + 1] |= ((bo << 4) & 61440) >> x;
+			f->bgrid[y + 2] |= ((bo << 8) & 61440) >> x;
+			f->bgrid[y + 3] |= ((bo << 12) & 61440) >> x;
 			elem->placed = 1;
 			return (1);
 		}
-		p++;
 	}
 	return (0);
 }
