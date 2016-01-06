061fce66ed110d07009f5d984e13c5a709559013
Chr0nos <snicolet@student.42.fr>
2016-01-06 23:01:55 +0000

    opti du trouvator

--- trouvator.c
+++ trouvator.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 15:03:43 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/06 22:47:42 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 22:54:26 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,46 +33,46 @@ static int	grid_extend(t_fillit *x)
 	return (1);
 }
 
-static int	trouvator_o_matic(t_fillit *f, t_point p, t_element *elem,
-		unsigned int n)
+static int	trouvator_o_matic(t_fillit *f, t_element *elem,	unsigned int n)
 {
-	while ((p.y < (int)f->grid_size) && (!(p.x = 0)))
+
+	int	x;
+	int	y;
+
+	y = 0;
+	while ((y < (int)f->grid_size) && (!(x = 0)))
 	{
-		while (p.x < (int)f->grid_size)
+		while (x < (int)f->grid_size)
 		{
-			if (insert_bin(f, p.x, p.y, elem) != 0)
+			if (insert_bin(f, x, y, elem) != 0)
 			{
-				if (trouvator_engine(*f, n + 1, p.y))
+				if (trouvator_engine(*f, n + 1, y))
 					return (1);
 				removator(f, elem);
 			}
-			p.x++;
+			x++;
 		}
-		p.y++;
+		y++;
 	}
 	return (0);
 }
 
 int			trouvator_engine(t_fillit f, unsigned int n, int y)
 {
-	t_point			p;
 	unsigned int	piece;
 	t_element		*elem;
 
+	(void)y;
 	if (n == f.elements_count)
 	{
 		super_display_of_doom(&f);
-		//displayator(&f);
 		return (1);
 	}
-	p.y = y;
 	piece = 0;
 	while ((piece < f.elements_count) && (elem = &f.elems[piece]))
 	{
-		if ((!elem->placed) && (trouvator_o_matic(&f, p, elem, n) == 1))
+		if ((!elem->placed) && (trouvator_o_matic(&f, elem, n) == 1))
 			return (1);
-		p.y = 0;
-		p.x = 0;
 		piece++;
 	}
 	return (0);
