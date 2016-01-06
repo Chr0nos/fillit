eeea8b7fa2a48356454c86e284083262b04a34c7
Chr0nos <snicolet@student.42.fr>
2016-01-06 19:54:24 +0100

    optimisation

--- canfit.c
+++ canfit.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/28 13:59:10 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/06 19:30:37 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 19:50:03 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int			canfit(t_fillit *f, int x, int y, t_element *bloc)
 {
-	const unsigned short	mask = bloc->bin;
+	const unsigned short	*m = bloc->masks;
 
 	if (((bloc->height + y) <= (unsigned char)f->grid_size) &&
 			((bloc->width + x) <= (unsigned char)f->grid_size) &&
-			!(((mask & 61440) >> x) & f->bgrid[y]) &&
-			!((((mask << 4) & 61440) >> x) & f->bgrid[y + 1]) &&
-			!((((mask << 8) & 61440) >> x) & f->bgrid[y + 2]) &&
-			!((((mask << 12) & 61440) >> x) & f->bgrid[y + 3]))
+			!((m[0] >> x) & f->bgrid[y]) &&
+			!((m[1] >> x) & f->bgrid[y + 1]) &&
+			!((m[2] >> x) & f->bgrid[y + 2]) &&
+			!((m[3] >> x) & f->bgrid[y + 3]))
 		return (1);
 	return (0);
 }
--- insert.c
+++ insert.c
@@ -6,30 +6,26 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/24 17:33:28 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/06 19:31:08 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/06 19:48:04 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int						insert_bin(t_fillit *f, int x, int y, t_element *elem)
+int		insert_bin(t_fillit *f, int x, int y, t_element *elem)
 {
-	const unsigned short	bo = elem->bin;
+	const unsigned short	*m = elem->masks;
 
 	if (((size_t)y + elem->height <= f->grid_size) && (canfit(f, x, y, elem)))
 	{
-		f->bgrid[y] &= ((unsigned short)65535) << (16 - f->grid_size);
-		if (x > -1)
-		{
-			elem->pos = (unsigned short)((unsigned short)(y) << 8);
-			elem->pos |= (unsigned short)x;
-			f->bgrid[y] |= (bo & 61440) >> x;
-			f->bgrid[y + 1] |= ((bo << 4) & 61440) >> x;
-			f->bgrid[y + 2] |= ((bo << 8) & 61440) >> x;
-			f->bgrid[y + 3] |= ((bo << 12) & 61440) >> x;
-			elem->placed = 1;
-			return (1);
-		}
+		elem->pos = (unsigned short)((unsigned short)(y) << 8);
+		elem->pos |= (unsigned short)x;
+		f->bgrid[y] |= m[0] >> x;
+		f->bgrid[y + 1] |= m[1] >> x;
+		f->bgrid[y + 2] |= m[2] >> x;
+		f->bgrid[y + 3] |= m[3] >> x;
+		elem->placed = 1;
+		return (1);
 	}
 	return (0);
 }
