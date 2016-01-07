e488acff3c6bdef9c244ccbed37626670c643436
Chr0nos <snicolet@student.42.fr>
2016-01-07 16:51:31 +0100

    rectification d un message d erreur pour la moulinette

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 11:50:23 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/06 23:25:54 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/07 16:51:09 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ int				main(int ac, char **av)
 			ft_putendl("error");
 	}
 	else
-		ft_putendl("nombre de parametres invalides");
+		ft_putendl("error");
 	return (0);
 }
